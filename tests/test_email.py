f97090a4d1f2d5f6ba9a8be78657975ef4a0e0ef
Petr "Stone" Hracek <phracek@redhat.com>
2024-11-06 08:28:20 +0100

    Fix email building structure.
    Also tests should be present in case we use it.
    
    Signed-off-by: Petr "Stone" Hracek <phracek@redhat.com>

---
+++ tests/test_email.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from flexmock import flexmock
+
+from auto_merger.email import EmailSender
+
+
+def test_create_email_no_recepients():
+    es = EmailSender(recipient_email=[])
+    es.create_email_msg("something important")
+    assert es.mime_msg
+    assert es.mime_msg["From"] == "phracek@redhat.com"
+    assert "phracek@redhat.com" in es.mime_msg["To"]
+    assert "sclorg@redhat.com" in es.mime_msg["To"]
+    assert es.mime_msg["Subject"] == "something important"
+
+
+def test_create_email_with_recipients():
+    es = EmailSender(recipient_email=["foo@bar.com"])
+    es.create_email_msg("something important")
+    assert es.mime_msg
+    assert es.mime_msg["From"] == "phracek@redhat.com"
+    assert "phracek@redhat.com" in es.mime_msg["To"]
+    assert "sclorg@redhat.com" in es.mime_msg["To"]
+    assert "foo@bar.com" in es.mime_msg["To"]
+    assert es.mime_msg["Subject"] == "something important"
